0881f04780dbee12a3cbeb3a5c03fa94759b34c1
RosalesLuis123 <nacholuispalma@gmail.com>
2025-10-16 11:47:37 -0400

    .

--- anime-act-backend/index.js
+++ anime-act-backend/index.js
@@ -29,7 +29,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'anime_act',
-  password: process.env.DB_PASSWORD || 'Tu contrasena',
+  password: process.env.DB_PASSWORD || '10387136',
   port: process.env.DB_PORT || 5432,
 });
 
